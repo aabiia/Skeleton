182ba03f6dc8efcc2deb2fd4e9d7b27995ac4778
Ahsan Sajjad <p2768416@my365.dmu.ac.uk>
2024-05-12 04:54:58 +0100

    Added testing  for the Stock Class.

---
+++ Testing3/tstStock.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstsanceOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            Assert.IsNotNull( AnStock );
+        }
+    }
+}
