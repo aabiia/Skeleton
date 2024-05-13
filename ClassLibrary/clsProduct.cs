03b45077614c5a3ba4ba08281acc9c8f4f453fcd
Ahsan Sajjad <p2768416@my365.dmu.ac.uk>
2024-05-13 04:54:27 +0100

    Successfull addesd

---
+++ ClassLibrary/clsProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsProduct
+    {
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Prod_Description { get; set; }
+        public float Prod_Price { get; set; }
+        public int Prod_Quantity { get; set; }
+        public DateTime Date_Added { get; set; }
+        public int Supplier_ID { get; set; }
+    }
+}
