10f45141abebd5844b648121c55768cb92c2de8c
P2750500 <abigail.adeniji@outlook.com>
2024-05-17 01:40:36 +0100

    p1-p8

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -14,6 +14,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
+
     protected void btnOK_Click(object sender, EventArgs e)
     {
         //create a new instance of clscustomer
